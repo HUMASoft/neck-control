47cc3d2664c54a54ec63755e4cac29e81adc6b2e
HumaSOFT <humasoft@ing.uc3m.es>
2018-07-09 13:39:33 +0200

    modificado:    main/openloop.cpp 	modificado:    main/position-control.cpp

--- main/openloop.cpp
+++ main/openloop.cpp
@@ -12,18 +12,18 @@ main ()
 {
 
     ofstream graph("graph.csv",std::ofstream::out);
-    SocketCanPort pm1("can0");
+    SocketCanPort pm1("can1");
     CiA402Device m1 (1, &pm1);
-    SocketCanPort pm2("can0");
+    SocketCanPort pm2("can1");
     CiA402Device m2 (2, &pm2);
-    SocketCanPort pm3("can0");
+    SocketCanPort pm3("can1");
     CiA402Device m3 (3, &pm3);
 
 
     TableKinematics a;
     vector<double> lengths(3);
     long orient=1;
-    long incli=20;
+    long incli=25;
 
     a.GetIK(incli,orient,lengths);
     cout << "l1 " << lengths[0]  << ", l2 " << lengths[1] << ", l3 " << lengths[2]<<endl;
@@ -33,13 +33,13 @@ main ()
     posan3=(0.1-lengths[2])*180/(0.01*M_PI);
     cout << "pos1 " << posan1  << ", pos2 " << posan2 << ", pos3 " << posan3;
 
-    m1.Reset();
-    m2.Reset();
-    m3.Reset();
+//    m1.Reset();
+//    m2.Reset();
+//    m3.Reset();
 
-    m1.SwitchOn();
-    m2.SwitchOn();
-    m3.SwitchOn();
+//    m1.SwitchOn();
+//    m2.SwitchOn();
+//    m3.SwitchOn();
 
     m1.SetupPositionMode(360,360);
     m2.SetupPositionMode(360,360);
@@ -48,7 +48,7 @@ main ()
     for (int i=0;i<17;i++)
     {
 
-        orient += 20;
+        orient += 5;
 
         a.GetIK(incli,orient,lengths);
         cout << "l1 " << lengths[0]  << ", l2 " << lengths[1] << ", l3 " << lengths[2]<<endl;
@@ -67,7 +67,7 @@ main ()
 
         //sleep(2);
         double dts=0.01;
-        for (double t=0;t<2; t+=dts)
+        for (double t=0;t<1; t+=dts)
         {
             usleep(dts*1000*1000);
             cout << t << " , " << m1.GetPosition() << " , " << m2.GetPosition() <<  " , " << m3.GetPosition() <<endl;
--- main/position-control.cpp
+++ main/position-control.cpp
@@ -6,11 +6,11 @@
 
 main ()
 {
-    SocketCanPort pm1("can0");
+    SocketCanPort pm1("can1");
     CiA402Device m1 (1, &pm1);
-    SocketCanPort pm2("can0");
+    SocketCanPort pm2("can1");
     CiA402Device m2 (2, &pm2);
-    SocketCanPort pm3("can0");
+    SocketCanPort pm3("can1");
     CiA402Device m3 (3, &pm3);
     m1.Reset();
     m2.Reset();
